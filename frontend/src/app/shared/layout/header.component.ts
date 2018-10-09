fa5a15584dc886ec1780ef12deada022d0fae584
Alenorze <alenorze@gmail.com>
2018-10-09 23:40:40 +0300

    Part 8

--- frontend/src/app/shared/layout/header.component.ts
+++ frontend/src/app/shared/layout/header.component.ts
@@ -10,6 +10,13 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function(){
+        $("button").click(function(){
+            var div = $("div");  
+            div.animate({left: '100px'}, "slow");
+            div.animate({fontSize: '5em'}, "slow");
+        });
+    });
   }
 
 }
